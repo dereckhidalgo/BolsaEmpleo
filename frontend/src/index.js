b93ccdcb88652b28e8cb6ff78f04d4e47fe55340
Jenrry <jenrrymonegro58@gmail.com>
2021-06-14 20:01:43 -0400

    Crud de Vacantes en Admin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import Admin from './admin/admin'
 import Create from './admin/create'
+import Admin_Vacante from './admin/vacantes'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Create />
+    <Admin_Vacante />
   </React.StrictMode>,
   document.getElementById('root')
 );
