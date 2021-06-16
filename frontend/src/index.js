69fe05dab9f3abb4d22fd1cab0b7be4839a0c560
Dereck Hidalgo <dereckhidalgo27@gmail.com>
2021-06-15 20:37:16 -0400

    Revert "iniciando el front desde cero"
    This reverts commit dfb2e318339d2e067b0bfdb6b9fa8b2fc094f8fa.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Inicio from './admin/admin';
+import Inicio from './Inicio/Inicio';
 
 
 
@@ -12,3 +12,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
