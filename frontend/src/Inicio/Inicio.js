49080d2110594046f872ea1d8c09679be995d162
Dereck Hidalgo <dereckhidalgo27@gmail.com>
2021-06-28 02:29:13 -0400

    Modal Postular para una vacante, inicio

--- frontend/src/Inicio/Inicio.js
+++ frontend/src/Inicio/Inicio.js
@@ -1,12 +1,24 @@
 import React,{useEffect,useState} from 'react';
-import { Navbar, Nav, Form, FormControl, Button,Modal,ModalBody, ModalFooter } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import {Modal, ModalBody,ModalFooter,ModalHeader} from 'reactstrap'
 import Portada from '../imgs/Portada.jpg';
 import Logo from '../imgs/Logo.png';
-import {Link} from "react-router-dom";
 import axios from 'axios';
+import Footer from '../Components/Footer';
+import swal from 'sweetalert';
+
+
 function Inicio() {
     const URL = "https://localhost:44375/api/Vacante";
+    const URL2 = "https://localhost:44375/api/Postulaciones";
 
+    const Alerta=()=>{
+        swal({
+            title:"Solicitud enviada!",
+            icon:"success",
+            button:"Aceptar",
+        })
+    }
     const [data, setData]=useState([]);
     const [VacanteSeleccionada, setVacanteSeleccionada]=useState({
         id: '',
@@ -15,6 +27,41 @@ function Inicio() {
     });
     const [modalInsertar,setModalInsertar]=useState(false);
     const [modalEditar,setModalEditar]=useState(false);
+    const [modalDetalles, setModalDetalles]=useState(false);
+    const [modalPostular, setModalPostular]=useState(false);
+
+    const [gestorseleccionado, setGestorseleccionado] = useState({
+        id:'',
+        compania:'',
+        tipo:'',
+        urL_LOGO:'',
+        Url_O:'',
+        posicion:'',
+        iD_CATEGORIA_FK:'',
+        ubicacion:'',
+        descripcion:'',
+        fechA_PUBLICACION:'',
+        iD_USUARIO_FK:'',
+        iD_VACANTE_FK:'',
+        fechA_POSTULACION:''
+
+    })
+    const seleccionarGestor=(gestorr,caso)=>{
+        setGestorseleccionado(gestorr);
+        if(caso==="Detalles"){
+            controlModalDetalles();
+        }
+    }
+    const controlModalDetalles=()=>{
+        setModalDetalles(!modalDetalles)
+       }
+
+    const controlModalPostular=()=>{
+        setModalPostular(!modalPostular)
+        if(modalDetalles==true){
+            setModalDetalles(!modalDetalles)
+        }
+       }
 
     const ToggleModal =()=>{
         setModalInsertar(!modalInsertar);
@@ -32,7 +79,27 @@ function Inicio() {
           console.log(error);   
         })
       }
-
+      const peticionPost= async()=>{
+        gestorseleccionado.iD_USUARIO_FK=parseInt(gestorseleccionado.iD_USUARIO_FK);
+        gestorseleccionado.iD_VACANTE_FK=parseInt(gestorseleccionado.id);
+        delete gestorseleccionado.id;      
+        await axios.post(URL2, gestorseleccionado)
+        .then(response =>{
+            setData(data.concat(response.data));
+            Alerta();
+            controlModalPostular();
+        }).catch(error=>{
+            console.log(error)
+        });
+    }
+      const handleChange=e=>{
+        const {name,value}=e.target;
+        setGestorseleccionado({
+            ...gestorseleccionado,
+            [name]:value
+        })
+        console.log(gestorseleccionado);
+    }
       const ControlarInputs =e=>{
         const {name, value}=e.target;
           setVacanteSeleccionada({
@@ -48,24 +115,25 @@ function Inicio() {
 
     return (
       <>
-        <Navbar bg="dark" variant="dark" style={{width:"100%",margin:"0",borderRadius:"0"}}>
+        <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home" style={{marginLeft:'1%', marginTop:'-10px'}}><img src={Logo} width="90px" height="70px"/></Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Inicio</Nav.Link>
                 <Nav.Link href="#features">Vacantes</Nav.Link>
             </Nav>
             <Nav style={{marginLeft:"50%"}}>
-                <Link to="/Login"><Button className="bg-dark">Login</Button></Link>
-                <Button className="bg-dark">Sign Up</Button>
+                <Button className="bg-dark" variant="outline-info">Login</Button>
+                <Button className="bg-dark" variant="outline-info">Sign Up</Button>
             </Nav>
             <Form className="inline" style={{display:'flex', marginLeft:"1%"}}>
-                <FormControl type="text" placeholder="Buscar..." style={{marginRight:'2%', border:'2px solid #19A7AE'}}/>
-                <Button variant="outline-info">Buscar</Button>
+                <FormControl type="text" placeholder="Buscar..." style={{marginRight:'2%', border:'2px solid #19A7AE'}} />
+                <Button variant="outline-primary">Buscar</Button>
             </Form>
         </Navbar>
-        <div style={{height:'44.3rem'}}>
-            <img src={Portada} width='100%' height='100%' style={{opacity:""}}></img>
-            <div style={{top:'86px', position:'absolute',width:'100%', background:'black',opacity:'50%',height:"89.2%"}}>
+        <div >
+            <img src={Portada} width='100%' style={{opacity:""}}></img>
+            <div style={{top:'86px', position:'absolute',width:'100%', background:'black',opacity:'50%',height:"90.2%"}}>
+                
             </div>
             <div style={{top:"40%",left:"30%",position:'absolute', color:"white"}}>
                 <h1 style={{color:"white"}}>TU <i style={{color:'#19A7AE'}}>FUTURO</i> EMPIEZA AQUI</h1>
@@ -93,7 +161,7 @@ function Inicio() {
                         <td>{dato.compania}</td>
                         <td>{dato.ubicacion}</td>
                         <td>{dato.posicion}</td>
-                        <td><button className="btn btn-info">Ver más...</button> {""}</td>
+                        <td><button className="btn btn-info" onClick={()=>seleccionarGestor(dato,"Detalles")}>Ver más...</button> {""}</td>
                     </tr>
                     :""
                 ))}
@@ -119,7 +187,7 @@ function Inicio() {
                         <td>{dato.compania}</td>
                         <td>{dato.ubicacion}</td>
                         <td>{dato.posicion}</td>
-                        <td><button className="btn btn-info">Ver más...</button> {""}</td>
+                        <td><button className="btn btn-info"  onClick={()=>seleccionarGestor(dato,"Detalles")}>Ver más...</button> {""}</td>
                     </tr>
                     :""
                 ))}
@@ -145,26 +213,119 @@ function Inicio() {
                         <td>{dato.compania}</td>
                         <td>{dato.ubicacion}</td>
                         <td>{dato.posicion}</td>
-                        <td><button className="btn btn-info">Ver más...</button> {""}</td>
+                        <td><button className="btn btn-info"  onClick={()=>seleccionarGestor(dato,"Detalles")}>Ver más...</button> {""}</td>
                     </tr>
                     :""
                 ))}
             </tbody>
         </table>
-    <Modal isOpen={modalInsertar}>
-      <ModalBody>
-        <div className="form-group">
-          <label>Titulo:</label>
-          <input type="text" name="titulo" className="form-control text-dark" placeholder="Ex: Liga de la justicia" onChange={ControlarInputs}/>
-          <textarea rows="4" cols="50" name="descripcion" className="form-control text-dark" placeholder="Ex: La pelicula trata de..." onChange={ControlarInputs}/>
-        </div>
-      </ModalBody>
+
+        
+    <Modal isOpen={modalPostular}>
+        <ModalHeader>
+        <h3 style={{marginLeft:'42%',fontFamily:'monospace',color:'#19A7AE'}}> Postulate</h3>
+
+        </ModalHeader>
+        
+        <ModalBody>
+        <div className="form-group row">
+                      <div class="form-group col-md-6">
+                      <label>Usuario</label>
+                      <br/>
+                      <input type="text" className="form-control" name="iD_USUARIO_FK" onChange={handleChange}/>
+                      </div>
+                      <div class="form-group col-md-6">
+                      <label>Vacante</label>
+                      <br/>
+                      <label>{gestorseleccionado && gestorseleccionado.id}</label>
+                      </div>
+                      <br></br>
+                      <div class="form-group col-md-6">
+                      <label>Fecha Postulacion</label>
+                      <br/>
+                      <input type="date" className="form-control" name="fechA_POSTULACION" onChange={handleChange}/>
+                      </div>
+            </div>
+        </ModalBody>
       <ModalFooter>
-        <button className="btn btn-info"><i className="tim-icons icon-check-2"/> Insertar</button>
-        <button className="btn btn-danger" onClick={()=>ToggleModal()}><i className="tim-icons icon-simple-remove"/> Cancelar</button>
+            <button className="btn btn-primary" onClick={()=>peticionPost()}>Guardar</button> {""}
+             <button className="btn btn-danger" onClick={()=>controlModalPostular()}>Cancelar</button>
       </ModalFooter>
     </Modal>
+
+
+    <Modal isOpen={modalDetalles} className="modal-lg 3500px">
+           <ModalHeader>
+               <center>
+                   <h3 style={{marginLeft:'42%',fontFamily:'monospace',color:'#19A7AE'}}> Vacantes Hiden</h3>
+               </center>
+           </ModalHeader>
+            <ModalBody>
+            <div className="form-group row">
+                  
+                      <div class="form-group col-md-6">
+                     <h2>Compania</h2>
+                      <br/>
+                      <label>{gestorseleccionado && gestorseleccionado.compania}</label>
+                      </div>
+
+                      <div class="form-group col-md-6">
+                      <br/>
+                      <img width="150" height="100" src={gestorseleccionado && gestorseleccionado.urL_LOGO}/>
+                      </div>
+                      
+                      <br></br>
+                      <div class="form-group col-md-6">
+                      <h4>Tipo</h4>
+                      
+                      <label>{gestorseleccionado && gestorseleccionado.tipo}</label>
+                      </div>
+                      
+                      <br></br>
+                      <div class="form-group col-md-6">
+                      <h4>Url_O</h4>
+                    
+                      <input type="text" className="form-control" name="Url_O"   value ={gestorseleccionado && gestorseleccionado.Url_O}/>
+                      </div>
+                      <br></br>
+                      <div class="form-group col-md-6">
+                      <h4>Posicion</h4>
+                      
+                      <label>{gestorseleccionado && gestorseleccionado.posicion}</label>
+                      </div>
+                      <div class="form-group col-md-6">
+                      <h4>Categoria</h4>
+                      
+                      <label>{gestorseleccionado && gestorseleccionado.iD_CATEGORIA_FK}</label>
+                      </div>
+                      <br></br>
+                      <div class="form-group col-md-6">
+                      <h4>Ubicacion:</h4>
+                      
+                     <label>{gestorseleccionado && gestorseleccionado.ubicacion}</label>
+                      </div>
+                      <div class="form-group col-md-6">
+                      <h4>Descripcion:</h4>
+                      
+                      <label>{gestorseleccionado && gestorseleccionado.descripcion}</label>
+                      </div>
+                      <br></br>
+                    
+                    <div class="form-group col-md-6">
+                      <h4>Fecha de Publicacion</h4>
+                      <label>{gestorseleccionado && gestorseleccionado.fechA_PUBLICACION}</label>
+                      </div>
+                      </div>
+            </ModalBody>
+
+            <ModalFooter>
+              <button className="btn btn-primary"onClick={()=>controlModalPostular()} >Postular</button> {""}
+             <button className="btn btn-danger" onClick={()=>controlModalDetalles()}>Cancelar</button>
+            </ModalFooter>
+    </Modal>
+
         </div>
+        <Footer/>
       </>
     );
   }
